fdfb5e9f8404f5fc9feb25665177550f141e82a4
sandeepkengal reddy <sandeepkengal1@gmail.com>
2024-01-31 10:56:08 +0000

    added notifications feature

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <HomeFeedPage />
   },
+  {
+    path: "/notifications",
+    element: <NotificationsFeedPage />
+  },  
   {
     path: "/@:handle",
     element: <UserFeedPage />
