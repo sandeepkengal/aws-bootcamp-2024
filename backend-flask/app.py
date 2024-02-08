fc829982d60bb2fd19c94a8abde0e39cccb6d156
sandeepkengal reddy <sandeepkengal1@gmail.com>
2024-02-08 08:16:53 +0000

    abc

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,6 +15,11 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# JWT
+from flask_cors import CORS, cross_origin
+
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 
 
 # HONEYCOMB
@@ -68,6 +73,12 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
 
 # X-ray -----
 XRayMiddleware(app, xray_recorder)
@@ -135,7 +146,19 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
